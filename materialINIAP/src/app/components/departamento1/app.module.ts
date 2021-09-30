97099d46acfcea82a359e5e3a3f12f1872931935
Jonas Villacis <81389379+LSyaRE@users.noreply.github.com>
2021-09-30 07:58:47 -0500

    Update app.module.ts
    Se arreglo el problema de las importaciones del module

--- materialINIAP/src/app/components/departamento1/app.module.ts
+++ materialINIAP/src/app/components/departamento1/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
+
+import { Departamento10Component } from '../departamento10/departamento10.component';
+import { Departamento11Component } from '../departamento11/departamento11.component';
+import { Departamento12Component } from '../departamento12/departamento12.component';
+import { Departamento13Component } from '../departamento13/departamento13.component';
+import { Departamento2Component } from '../departamento2/departamento2.component';
+import { Departamento3Component } from '../departamento3/departamento3.component';
+import { Departamento4Component } from '../departamento4/departamento4.component';
+import { Departamento5Component } from '../departamento5/departamento5.component';
+import { Departamento6Component } from '../departamento6/departamento6.component';
+import { Departamento7Component } from '../departamento7/departamento7.component';
+import { Departamento8Component } from '../departamento8/departamento8.component';
+import { Departamento9Component } from '../departamento9/departamento9.component';
+import { InventarioComponent } from '../inventario/inventario.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Departamento2Component } from './departamento2/departamento2.component';
-import { Departamento3Component } from './app/departamento3/departamento3.component';
-import { Departamento4Component } from './app/departamento4/departamento4.component';
-import { Departamento5Component } from './app/departamento5/departamento5.component';
-import { Departamento6Component } from './app/departamento6/departamento6.component';
-import { Departamento7Component } from './app/departamento7/departamento7.component';
-import { Departamento8Component } from './app/departamento8/departamento8.component';
-import { Departamento9Component } from './app/departamento9/departamento9.component';
-import { Departamento10Component } from './app/departamento10/departamento10.component';
-import { Departamento11Component } from './app/departamento11/departamento11.component';
-import { Departamento12Component } from './app/departamento12/departamento12.component';
-import { Departamento13Component } from './app/departamento13/departamento13.component';
-import { InventarioComponent } from './inventario/inventario.component';
+
 
 @NgModule({
   declarations: [

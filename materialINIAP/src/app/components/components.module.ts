851853995d200448859537a05d233edba64e1869
Jonas Villacis <81389379+LSyaRE@users.noreply.github.com>
2021-09-30 10:56:39 -0500

    eliminando para solo usar

--- materialINIAP/src/app/components/components.module.ts
+++ materialINIAP/src/app/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InventarioComponent } from './inventario/inventario.component';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports:[
+   InventarioComponent
   ]
 })
 export class ComponentsModule { }

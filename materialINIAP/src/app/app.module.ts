71bf7eca31fe0195cce365d18b920bf5762889d3
Jonas Villacis <81389379+LSyaRE@users.noreply.github.com>
2021-10-03 06:34:26 -0500

    recomposicion de las conexiones
    se volvio a montar la estructura del angular para su buen uso

--- materialINIAP/src/app/app.module.ts
+++ materialINIAP/src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components.module';
-import { HomeComponent } from './pages/home/home.component';
-
-
 
 @NgModule({
   declarations: [
     AppComponent
-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ComponentsModule,
-    HomeComponent
-
-  ]
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }

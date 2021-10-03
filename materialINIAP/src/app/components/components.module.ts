af74cd5aa6eaa2d1cb9959c9fbd4c8b44e2620e7
Jonas Villacis <81389379+LSyaRE@users.noreply.github.com>
2021-10-03 06:55:47 -0500

    conexiones hechas
    se comprobo que las conexiones funcionan correctamente ademas de comprobar el funcionamiento del bootstrap

---
+++ materialINIAP/src/app/components/components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Departamento1Component } from './departamento1/departamento1.component';
+
+
+
+@NgModule({
+  declarations: [
+    Departamento1Component
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+Departamento1Component
+
+  ]
+})
+export class ComponentsModule { }

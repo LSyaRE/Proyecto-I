ba5c1a0f1bec09b86918e7937735825014a1d4aa
Jonas Villacis <81389379+LSyaRE@users.noreply.github.com>
2021-10-03 20:05:33 -0500

    hola
    hola

--- materialINIAP/src/app/app.module.ts
+++ materialINIAP/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,

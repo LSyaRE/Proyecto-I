a8f780b41120f635562db605aac8ecb465448d06
LSyaRE <81389379+LSyaRE@users.noreply.github.com>
2021-10-04 16:11:04 -0500

    conexiones con la otra pagina
    se hizo las conexiones necesarias para el funcionamiento de la pagina

--- materialINIAP/src/app/app-routing.module.ts
+++ materialINIAP/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',component:LoginComponent},
+{path:'home',component:NavegacionComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

64c807fe5f635e85f92c57a5e395c8bfddafce51
Jonas Villacis <81389379+LSyaRE@users.noreply.github.com>
2021-10-11 10:14:07 -0500

    hola

--- materialINIAP/src/app/components/components-routing.module.ts
+++ materialINIAP/src/app/components/components-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavegacionComponent } from './navegacion/navegacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'home',component:NavegacionComponent},
+{path:'',redirectTo:'/home',pathMatch:'full'},
+{path:'**',redirectTo:'/home',pathMatch:'full' }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

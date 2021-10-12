72d80c284ba2c56e56ee89862e5bc5bbc0e5df43
LSyaRE <81389379+LSyaRE@users.noreply.github.com>
2021-10-12 11:27:35 -0500

    asdasd
    asdasd

--- materialINIAP/src/app/components/navegacion/navegacion.component.ts
+++ materialINIAP/src/app/components/navegacion/navegacion.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class NavegacionComponent implements OnInit {
 
   nombre: string = 'Direccion General'
+  opcion: string = '';
   
   constructor(private router:Router) { }
 

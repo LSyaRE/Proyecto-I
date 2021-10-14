0c5226501e57d87cc23940c1908f3ba789c6047d
LSyaRE <81389379+LSyaRE@users.noreply.github.com>
2021-10-14 09:51:16 -0500

    hi

---
+++ base/database.sql
@@ -0,0 +1,24 @@
+create database INIAP;
+
+create table prueba(
+
+    id_prueba int,
+    nombre varchar(50),
+    descripcion varchar(100)
+);
+
+INSERT INTO prueba VALUES (1,'Prueba1','se hizo una prueba de la base de datos');
+
+select * from prueba;
+
+create table users(
+
+    id_user int,
+    nombre varchar(50),
+    password_user varchar(100)
+);
+
+INSERT INTO users VALUES 
+(1,'Jonas','INIAP2021',
+(2,'Valeria','INIAP2021',
+(3,'Joshua','INIAP2021',;

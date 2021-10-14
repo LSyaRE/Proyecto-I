d86fd21dd30877c67a325fbb12c6c4dfed8c01c8
LSyaRE <81389379+LSyaRE@users.noreply.github.com>
2021-10-14 11:08:39 -0500

    Update index.js

--- base/index.js
+++ base/index.js
@@ -1,1 +1,46 @@
-console.log('Hello World')
+const { Pool}= require('pg')
+
+const config ={
+    user: 'postgres',
+    host: 'localhost',
+    password:'12345',
+    database:'iniap'
+};
+
+const pool= new Pool(config);
+
+const getUsers = async () => {
+
+    try {
+    const res= await pool.query('select * from users');
+    console.log(res.rows)
+    // pool.end(); acaba la conexion
+
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+    
+}
+
+const insertUsers = async () => {
+
+    try {
+
+    const text=  'INSERT INTO users VALUES ($1,$2,$3);'
+    const values= ['10','Alan','INIAP2021'] 
+    const res= await pool.query(text,values);
+
+    console.log(res)
+    // pool.end(); acaba la conexion
+
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+    
+}
+insertUsers()
+getUsers()

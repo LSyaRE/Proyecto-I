70025e9e4f32a941488cf5b7d83ae31102777e9c
Jonas Villacis <81389379+LSyaRE@users.noreply.github.com>
2021-10-15 17:58:15 -0500

    Update rutas.js

--- base/rutas.js
+++ base/rutas.js
@@ -1,8 +1,14 @@
 const rutas  = require('express').Router();
+
 const conexion = require('./config/conexion')
 
 //----------Asignacion de Rutas------------
 
+rutas.get('/',function(req,res){
 
+    res.send('Hola Valeria xD');
+}
+
+)
 //-----------------------------------------
 module.exports= rutas;                                                                                                                                                                           

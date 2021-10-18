4740359627c3afdeb448cde85086a33bff9f5a94
LSyaRE <81389379+LSyaRE@users.noreply.github.com>
2021-10-18 10:06:04 -0500

    metodo no funcional

--- materialINIAP/src/app/components/navegacion/navegacion.component.ts
+++ materialINIAP/src/app/components/navegacion/navegacion.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ConnbdService } from 'src/app/services/connbd.service';
+
 
 
 @Component({
@@ -13,19 +12,13 @@ export class NavegacionComponent implements OnInit {
   nombre: string = 'Direccion General'
   opcion: string = '';
 
-  constructor(private connbdService: ConnbdService) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  listarUser() {
-    this.connbdService.getUsers().subscribe(
-      res => {
-        console.log(res)
-      },
-      err => console.log(err)
-    );
+
   }
 
 
-}
+

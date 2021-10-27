5e89f988736dd7daea60fa3b3bd623cd2a08b63e
Jonas Villacis <81389379+LSyaRE@users.noreply.github.com>
2021-10-27 13:58:06 -0500

    Update departamento1.component.ts

--- materialINIAP/src/app/components/departamento1/departamento1.component.ts
+++ materialINIAP/src/app/components/departamento1/departamento1.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Persona } from 'src/app/interface/personas/persona';
+import { CrudService } from 'src/app/services/crud/crud.service';
 
 @Component({
   selector: 'app-departamento1',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Departamento1Component implements OnInit {
 
-  constructor() { }
+  //Llama al modulo de interfaz de usuario y lo almacena
+  listado:Persona[] = [];
 
+  //Dentro del constructor llama al servicio del crud
+  constructor(private crudService:CrudService,private router:Router) { }
+
+  //esta funcion hace que empiece a ejecutar  la funcion de listar Equipo
   ngOnInit(): void {
+    this.listarEquipo();
   }
+
+  //Delay para que se autorecarguen las tablas
+  delay(n:any){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
+}
+
+  
+  //Esta funcion nos permite usar la funcion dentro de CrudService y inicio la funcion de GetUsuarios ademas 
+  //entrega la respuesta y la almacena en listadoUsuario
+listarEquipo()
+{
+  this.crudService.getUsuarios().subscribe(
+    res=> {
+      
+      console.log(res)
+      this.listado=<any>res; 
+      
+    },
+    err=> console.log(err)
+  );
+};
+
+async eliminarUsuario(id_delete:any){
+  this.crudService.deleteUsuario(id_delete).subscribe(
+    res=>{
+      
+      console.log('Equipo eliminado')
+      
+    },
+    err=> console.log(err)
+  );
+  await this.delay(0.5)
+  this.listarEquipo();
+};
+
+
+editarUsuario(id_edit:any){
+  this.router.navigate(['/editar/'+id_edit])
+}
 
 }
